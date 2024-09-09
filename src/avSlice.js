db9060a9045081991b55b6f1ad147d418c62fc3e
carina1196 <carinachanqingwei@gmail.com>
2024-09-10 01:00:13 +0800

    updated codes with required reducers etc

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,51 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
-  ],
+    {
+      img: "public\\assets\\projector.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      img: "public\\assets\\speakers.jpg",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      img: "public\\assets\\microphones.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      img: "public\\assets\\whiteboard.jpg",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
 
+    {
+      img: "public\\assets\\signs.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
+  ],
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
     },
     decrementAvQuantity: (state, action) => {
-     
+      const item = state[action.payload];
+      if (item && item.quantity > 0) {
+        item.quantity--;
+      }
     },
   },
 });
